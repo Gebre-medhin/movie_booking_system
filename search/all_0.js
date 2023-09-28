d3c8cceaa1eda31e1e52540bea3beb86087b71a1
Gebre-medhin <Gebre-medhin@users.noreply.github.com>
2023-09-28 18:54:03 +0000

    deploy: 7ba1641d630e114716a5361283152bac6be58670

--- search/all_0.js
+++ search/all_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['addmovie_0',['addMovie',['../classMovieBookingService.html#a22eba0c7b5c896254c6a4389c2610ab0',1,'MovieBookingService']]],
   ['addtheater_1',['addTheater',['../classMovieBookingService.html#ae9d6a885a79a621f6a32d5d7229812a4',1,'MovieBookingService']]],
-  ['allocatemovietotheaters_2',['allocateMovieToTheaters',['../classMovieBookingService.html#a8ffea7113dedf9b770e38e301b04009c',1,'MovieBookingService']]]
+  ['allocatemovietotheaters_2',['allocateMovieToTheaters',['../classMovieBookingService.html#a0a94244ec8d54cfb21f94a91a425f05a',1,'MovieBookingService']]]
 ];
